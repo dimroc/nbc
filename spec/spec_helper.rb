30f294a364b7fda12ca3207b7e198635f7e9423b
Dimitri Roche <dimroc@gmail.com>
2012-06-07 21:09:35 -0400

    Refactor database cleaner into support file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,39 +17,11 @@ RSpec.configure do |config|
   config.include(EmailSpec::Matchers)
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+  config.global_fixtures = :all
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-
-  # Clean up the database
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 end
