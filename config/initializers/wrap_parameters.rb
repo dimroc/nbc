94ba2d717908dc2d31586972d325822b426c12dc
Dimitri Roche <dimroc@gmail.com>
2012-06-15 19:26:34 -0400

    Unwrapped json parameters in Rails for Spine

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -6,5 +6,10 @@
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
   wrap_parameters format: [:json]
+
+  # Disable root element in JSON by default.
+  if defined?(ActiveRecord)
+    ActiveRecord::Base.include_root_in_json = false
+  end
 end
 
