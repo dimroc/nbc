4f3f1422b741f4f929008872347a19ddd43923aa
Dimitri Roche <dimroc@gmail.com>
2012-06-15 18:13:20 -0400

    Add region new york city to fixtures

--- spec/support/fixture_builder.rb
+++ spec/support/fixture_builder.rb
@@ -2,6 +2,10 @@ FixtureBuilder.configure do |fbuilder|
   fbuilder.files_to_check += Dir["spec/factories/**/*.rb", "spec/support/fixture_builder.rb"]
 
   fbuilder.factory do
+    # Users
     fbuilder.name(:standard_user, FactoryGirl.create(:user))
+
+    # Regions
+    fbuilder.name(:nyc, FactoryGirl.create(:region, name: "New York City"))
   end
 end
