41d8460cc59038eeaf1b42455dea1b5618e47a4d
Dimitri Roche <dimroc@gmail.com>
2012-06-25 08:58:37 -0400

    Converted RegionGenerator to be a FactoryGirl

--- spec/factories/regions.rb
+++ spec/factories/regions.rb
@@ -1,5 +1,18 @@
 FactoryGirl.define do
   factory :region do
+    ignore do
+      width 10
+      height 10
+    end
+
     name { Faker::AddressUS.state }
+
+    after(:build) do |region, evaluator|
+      (0...evaluator.width).each do |left|
+        (0...evaluator.height).each do |top|
+          region.blocks << Block.new(left: left, top: top)
+        end
+      end
+    end
   end
 end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,8 +2,40 @@ require 'spec_helper'
 
 describe Region do
   describe "factories" do
-    it "should build a valid region" do
-      FactoryGirl.build(:region).should be_valid
+    context ".region" do
+      shared_examples_for "a valid region" do
+        it "should generate the region" do
+          expected_count = width * height
+          expect { region.save }.to change { Region.count }.by(1)
+          region.blocks.count.should == expected_count
+
+          (0..width).each do |left|
+            (0..height).each do |top|
+              region.blocks.any? { |block| block.left == left && block.top == top }
+            end
+          end
+        end
+      end
+
+      context "with default values" do
+        let(:region) { FactoryGirl.build(:region) }
+        let(:width) { 10 }
+        let(:height) { 10 }
+        it_should_behave_like "a valid region"
+      end
+
+      context "with set values" do
+        let(:region) { FactoryGirl.build(:region, name: region_name, width: width, height: height) }
+        let(:region_name) { "New York City" }
+        let(:width) { 19 }
+        let(:height) { 7 }
+
+        it "should assign the name" do
+          region.name.should == region_name
+        end
+
+        it_should_behave_like "a valid region"
+      end
     end
   end
 
