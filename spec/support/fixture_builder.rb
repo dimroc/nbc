64ba038030176ad8dd1cc68c7d625321ee478471
Dimitri Roche <dimroc@gmail.com>
2012-11-13 09:10:59 -0500

    Add neighborhoods to fixtures

--- spec/support/fixture_builder.rb
+++ spec/support/fixture_builder.rb
@@ -5,9 +5,13 @@ FixtureBuilder.configure do |fbuilder|
     # Users
     fbuilder.name(:standard_user, FactoryGirl.create(:user))
 
+    # Neighborhoods
+    Loader::Neighborhood.from_shapefile(
+      Rails.root.join("lib/data/shapefiles/neighborhoods/region.shp").to_s)
+
     # Worlds
     @nyc = Loader::World.from_shapefile(
-      "/Users/dimroc/workspace/new-block-city/lib/data/shapefiles/nyc/region.shp",
+      Rails.root.join("lib/data/shapefiles/nyc/region.shp").to_s,
       "BoroCD" => "name")
     @nyc.name = "NYC"
     @nyc.regenerate_blocks(3000)
