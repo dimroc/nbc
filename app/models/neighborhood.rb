1716a5775f530134098386a66b8b4035af633339
Dimitri Roche <dimroc@gmail.com>
2012-11-13 09:11:14 -0500

    Add for_region to Neighborhoods

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,3 +1,15 @@
 class Neighborhood < ActiveRecord::Base
   validates_presence_of :name, :borough, :point
+
+  class << self
+    def for_region(region)
+      find_by_sql(<<-SQL)
+      SELECT n.* AS result FROM neighborhoods n, regions r WHERE
+        ST_Contains(
+          ST_GeomFromEWKB(r.geometry),
+          ST_GeomFromEWKB(n.point))
+        AND r.id = #{region.id}
+      SQL
+    end
+  end
 end
--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -6,4 +6,17 @@ describe Neighborhood do
     it { should validate_presence_of :borough }
     it { should validate_presence_of :point }
   end
+
+  describe "for_region" do
+    subject { Neighborhood.for_region(region) }
+
+    context "integration" do
+      let(:included_neighborhood) { Neighborhood.first }
+      let(:region) { Region.create(name: "SomePoint", geometry: included_neighborhood.point) }
+
+      it "should return the relevant neighborhoods" do
+        subject.should == [included_neighborhood]
+      end
+    end
+  end
 end
