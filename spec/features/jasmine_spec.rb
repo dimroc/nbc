567605b94ccb11de381619e1b7894adecfa7b6bb
Dimitri Roche <dimroc@gmail.com>
2012-11-14 23:16:37 -0500

    capybara v2 removed wait_until, so use custom

--- spec/features/jasmine_spec.rb
+++ spec/features/jasmine_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 feature "JasmineSpecs" do
+  def wait_until
+    finished = false
+    iterations = 0
+    while(!finished && iterations < 5)
+      finished = yield
+      sleep(1)
+      iterations += 1
+    end
+  end
+
   scenario "should all pass", js: true do
     visit "/jasmine"
-    wait_until do
-      page.evaluate_script("jsApiReporter.finished")
-    end
+
+    wait_until { page.evaluate_script("jsApiReporter.finished") }
 
     jasmine_results =
       page.evaluate_script(<<-TRUTH_JS)
