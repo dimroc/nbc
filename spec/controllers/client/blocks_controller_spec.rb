6be869649188c9528e1334a193d255d666dc0d38
Dimitri Roche <dimroc@gmail.com>
2013-04-29 23:54:38 -0400

    Complete Client::BlocksController specs

--- spec/controllers/client/blocks_controller_spec.rb
+++ spec/controllers/client/blocks_controller_spec.rb
@@ -15,7 +15,22 @@ describe Client::BlocksController do
     context "with client api token" do
       before { request.env['NBC_SIGNATURE'] = 'yicceHasFatcowJemIvRurwojidfaitt' }
 
-      it "should create a block"
+      it "should create a block" do
+        params = {
+          latitude: 40.0,
+          longitude: -70.0,
+          direction: 90.0,
+          destinationUri: 'someUri'
+        }
+
+        PandaVideo.should_receive(:create_from_source).with('someUri').and_return(nil)
+        expect {
+          post :create, params
+        }.to change {Block::Video.count}.by(1)
+
+        block = Block.last
+        block.direction.should == 90.0
+      end
     end
   end
 end
