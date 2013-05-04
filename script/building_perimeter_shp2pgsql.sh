df218a62f1127aa786e10461879929e6bb142eec
Dimitri Roche <dimroc@gmail.com>
2013-05-04 17:16:41 -0400

    building_perimeter shpfile -> sql takes file parameter

--- script/building_perimeter_shp2pgsql.sh
+++ script/building_perimeter_shp2pgsql.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-shp2pgsql -c -D -s 3785 -I buildingPerimeters.shp public.building_perimeters  > building_perimeters.sql
+SHPFILE=$1
+
+if [ -z $SHPFILE ]
+then
+  echo 'No shapefile. args: <shpfile>'
+  exit
+fi
+
+shp2pgsql -c -D -s 3785 -I $SHPFILE public.building_perimeters  > building_perimeters.sql
+
+# After creating .sql file, pipe it into psql
+# eg: cat building_perimeters.sql | psql nbc_development
