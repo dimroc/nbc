d5b4bb11130bd74cc2181c503c9d13c8ee08be92
Dimitri Roche <dimroc@gmail.com>
2013-05-05 14:12:32 -0400

    Write neighborhoods' buildings to disk in seed

--- db/seeds/neighborhoods.rb
+++ db/seeds/neighborhoods.rb
@@ -3,3 +3,4 @@ TRUNCATE neighborhoods RESTART IDENTITY;
 SQL
 
 Loader::Neighborhood.from_shapefile("lib/data/shapefiles/neighborhoods/region")
+Loader::Neighborhood.write_building_perimeters_to_file
