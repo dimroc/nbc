3d50802ee69428957815c989f6f9fc4bb194a932
Dimitri Roche <dimroc@gmail.com>
2013-05-05 14:10:40 -0400

    Neighborhoods#building_perimeters retrieves containing buildings' geoJSON

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -12,11 +12,27 @@ class Neighborhood < ActiveRecord::Base
       where(<<-SQL, geom.as_text)
         ST_Intersects(ST_AsText(neighborhoods.geometry), ?)
       SQL
-      #where(<<-SQL)
-        #ST_Contains(
-          #ST_GeomFromText('#{geometry.as_text}',#{geometry.srid}),
-          #point)
-      #SQL
     end
+  end
+
+  def building_perimeters
+    JSON.parse(building_perimeters_json)
+  end
+
+  def building_perimeters_json
+    raise StandardError, "Neighborhood must be persisted" unless persisted?
+
+    # We only retrieve the first polygon of the building_perimeters table
+    # MultiPolygon when collecting all the building perimeters.
+    # This way, we can return one MultiPolygon.
+    rval = ActiveRecord::Base.connection.execute(<<-SQL)
+    CREATE TEMP TABLE temp_building_perimeters ON COMMIT DROP AS
+      (SELECT ST_GeometryN(bp.geom, 1) perimeter FROM building_perimeters bp, neighborhoods n
+        WHERE ST_Intersects(bp.geom, n.geometry) = true AND n.id = #{id});
+
+    SELECT ST_AsGeoJSON(ST_Collect(perimeter)) FROM temp_building_perimeters;
+    SQL
+
+    rval.values[0][0]
   end
 end
