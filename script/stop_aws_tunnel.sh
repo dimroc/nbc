099bfa342f25ad32efe8400c42d0f0873911601c
Dimitri Roche <dimroc@gmail.com>
2013-07-16 23:33:44 -0400

    Update scripts to use the was postgis server

--- script/stop_aws_tunnel.sh
+++ script/stop_aws_tunnel.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-aws stop i-af7478d0
-pid=`ps aux | ack ssh.*remote_awsmicro | ack -v perl | awk '{ print $2 }'`
+pid=`ps aux | ack ssh.*remote_awspostgis | ack -v perl | awk '{ print $2 }'`
 kill $pid
